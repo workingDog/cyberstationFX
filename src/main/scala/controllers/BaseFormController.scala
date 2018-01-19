6a36ebd912b1dd80787da1c245ae2672f4f5ffef
workingdog <ringowathelet@gmail.com>
2018-01-19 14:58:33 +0900

    initial commit

---
+++ src/main/scala/controllers/BaseFormController.scala
@@ -0,0 +1,115 @@
+package controllers
+
+import com.jfoenix.controls.{JFXButton, JFXListView}
+import cyber.CyberObj
+
+import scalafx.Includes._
+import scalafx.collections.ObservableBuffer
+import scalafx.scene.control.Label
+import scalafx.scene.control.cell.TextFieldListCell
+import scalafx.scene.input.MouseEvent
+import scalafx.util.StringConverter
+
+
+class BaseFormController(cyberType: String,
+                         formListView: JFXListView[CyberObj],
+                         addButton: JFXButton,
+                         deleteButton: JFXButton,
+                         bundleLabel: Label,
+                         commonController: CommonControllerInterface) {
+
+  val formList = ObservableBuffer[CyberObj]()
+  var bundleController: Option[BundleViewControllerInterface] = None
+
+  def init(): Unit = {
+    // setup the list of CyberObj
+    formListView.setEditable(true)
+    formListView.setExpanded(true)
+    formListView.setDepth(1)
+    formListView.setItems(formList)
+    // to edit the name in the list by double clicking on it
+    formListView.cellFactory = { _ =>
+      new TextFieldListCell[CyberObj] {
+        converter = formStringConverter
+        item.onChange { (_, _, obj) => if (obj != null) text = obj.name.value }
+      }
+    }
+    formListView.getSelectionModel.selectedItem.onChange { (_, oldValue, newValue) =>
+      // the commonController will take care of all interactions/updates for the common attributes
+      if (newValue != null) {
+        commonController.control(newValue, bundleController)
+      } else {
+        commonController.clear()
+      }
+    }
+  }
+
+  def setBundleViewController(controller: BundleViewControllerInterface): Unit = {
+    bundleController = Option(controller)
+    // this is to clear the list of CyberObj when the list of bundles is cleared
+    bundleController.map(controller => {
+      controller.getAllBundles().onChange((source, changes) => {
+        if (controller.getAllBundles().isEmpty) formList.clear()
+      })
+    })
+    // get the BundleViewController currentBundle
+    val currentBundle = controller.getCurrentBundle()
+    val bndlName = if (currentBundle.value != null) currentBundle.value.name.value else ""
+    bundleLabel.text = "Part of bundle: " + bndlName
+    if (currentBundle != null) {
+      currentBundle.onChange { (source, oldValue, newValue) =>
+        // load the new bundle list of CyberObj
+        if (newValue != null) {
+          val objForms = newValue.objects.filter(stix => stix.`type`.value == cyberType)
+          formList.clear()
+          formList.appendAll(objForms)
+          bundleLabel.text = "Part of bundle: " + newValue.name.value
+        } else {
+          bundleLabel.text = "Part of bundle: "
+        }
+      }
+    }
+  }
+
+  def doDelete(ev: MouseEvent) = {
+    val selectedItem = formListView.getSelectionModel.getSelectedItem
+    if (selectedItem != null) {
+      val ndx = formList.indexWhere(b => b.id == selectedItem.id)
+      if (ndx != -1) {
+        formList.remove(ndx)
+        formListView.getSelectionModel.clearSelection()
+        bundleController.map(_.removeStixFromBundle(selectedItem))
+      }
+    }
+  }
+
+  def doAdd(newStix: CyberObj) = {
+    bundleController.map(controller => {
+      if (controller.getCurrentBundle().value != null) {
+        formList.append(newStix)
+        controller.addStixToBundle(newStix)
+        formListView.getSelectionModel.select(newStix)
+      }
+    })
+  }
+
+  // for use in formListView, to allow editing (double click) of the list entries text, the CyberObj name
+  val formStringConverter = new StringConverter[CyberObj] {
+
+    def fromString(newName: String): CyberObj = {
+      val selectedStix = formListView.getSelectionModel.getSelectedItem
+      if (selectedStix != null) {
+        // remove the old stix from the bundleController
+        bundleController.map(_.removeStixFromBundle(selectedStix))
+        // put the new name in the selection
+        selectedStix.name.value = newName
+        // add the new selection to the bundleController
+        bundleController.map(_.addStixToBundle(selectedStix))
+      }
+      selectedStix
+    }
+
+    def toString(stix: CyberObj): String = stix.name.value
+  }
+
+}
--- src/main/scala/controllers/IndicatorController.scala
+++ src/main/scala/controllers/IndicatorController.scala
@@ -4,14 +4,11 @@ import javafx.fxml.FXML
 
 import com.jfoenix.controls.{JFXButton, JFXListView}
 import com.kodekutters.stix._
-import cyber.IndicatorForm
+import cyber.{CyberObj, IndicatorForm}
 
 import scalafx.Includes._
-import scalafx.collections.ObservableBuffer
 import scalafx.scene.control.Label
-import scalafx.scene.control.cell.TextFieldListCell
 import scalafx.scene.input.MouseEvent
-import scalafx.util.StringConverter
 import scalafxml.core.macros.{nested, sfxml}
 
 
@@ -21,112 +18,33 @@ trait IndicatorControllerInterface {
   def setBundleViewController(controller: BundleViewControllerInterface): Unit
 }
 
+
 @sfxml
 class IndicatorController(@FXML indicatorListView: JFXListView[IndicatorForm],
                           @FXML addButton: JFXButton,
-                          bundleLabel: Label,
                           @FXML deleteButton: JFXButton,
+                          bundleLabel: Label,
                           @nested[CommonController] commonController: CommonControllerInterface,
-                          @nested[IndicatorSpecController] indicatorSpecController: IndicatorSpecControllerInterface) extends IndicatorControllerInterface {
+                          @nested[IndicatorSpecController] indicatorSpecController: IndicatorSpecControllerInterface)
+  extends IndicatorControllerInterface {
 
-  val indicatorList = ObservableBuffer[IndicatorForm]()
-  var bundleController: Option[BundleViewControllerInterface] = None
+  val baseForm = new BaseFormController(Indicator.`type`, indicatorListView.asInstanceOf[JFXListView[CyberObj]], addButton, deleteButton, bundleLabel, commonController)
+  def setBundleViewController(controller: BundleViewControllerInterface): Unit = baseForm.setBundleViewController(controller)
+  deleteButton.setOnMouseClicked((ev: MouseEvent) => baseForm.doDelete(ev))
+  addButton.setOnMouseClicked((ev: MouseEvent) => baseForm.doAdd(new IndicatorForm()))
 
   init()
 
   def init(): Unit = {
-    // setup the list of indicators
-    indicatorListView.setEditable(true)
-    indicatorListView.setExpanded(true)
-    indicatorListView.setDepth(1)
-    indicatorListView.setItems(indicatorList)
-    // to edit the name in the list by double clicking on it
-    indicatorListView.cellFactory = { _ =>
-      new TextFieldListCell[IndicatorForm] {
-        converter = indicatorStringConverter
-        item.onChange { (_, _, obj) => if (obj != null) text = obj.name.value }
-      }
-    }
-    indicatorListView.getSelectionModel.selectedItem.onChange { (_, oldValue, newValue) =>
-      // the commonController and indicatorSpecController
-      // will take care of all interactions/updates
-      if (newValue != null) {
-        commonController.control(newValue, bundleController)
-        indicatorSpecController.control(newValue, bundleController)
-      } else {
-        commonController.clear()
-        indicatorSpecController.clear()
-      }
-    }
-  }
-
-  def setBundleViewController(controller: BundleViewControllerInterface): Unit = {
-    bundleController = Option(controller)
-    // this is to clear the list of indicators when the list of bundles is cleared
-    bundleController.map(controller => {
-      controller.getAllBundles().onChange((source, changes) => {
-        if (controller.getAllBundles().isEmpty) indicatorList.clear()
-      })
-    })
-    // the BundleViewController currentBundle
-    val currentBundle = controller.getCurrentBundle()
-    val bndlName = if (currentBundle.value != null) currentBundle.value.name.value else ""
-    bundleLabel.text = "Part of bundle: " + bndlName
-    if (currentBundle != null) {
-      currentBundle.onChange { (source, oldValue, newValue) =>
-        // load the new bundle list of IndicatorForm
+    baseForm.init()
+    indicatorListView.getSelectionModel.selectedItem.onChange {
+      (_, oldValue, newValue) =>
         if (newValue != null) {
-          val indicators = newValue.objects.filter(stix => stix.`type`.value == Indicator.`type`).asInstanceOf[ObservableBuffer[IndicatorForm]]
-          indicatorList.clear()
-          indicatorList.appendAll(indicators)
-          bundleLabel.text = "Part of bundle: " + newValue.name.value
+          indicatorSpecController.control(newValue, baseForm.bundleController)
         } else {
-          bundleLabel.text = "Part of bundle: "
+          indicatorSpecController.clear()
         }
-      }
     }
-  }
-
-  addButton.setOnMouseClicked((_: MouseEvent) => {
-    bundleController.map(controller => {
-      if (controller.getCurrentBundle().value != null) {
-        val newStix = new IndicatorForm()
-        indicatorList.append(newStix)
-        controller.addStixToBundle(newStix)
-        indicatorListView.getSelectionModel.select(newStix)
-      }
-    })
-  })
-
-  deleteButton.setOnMouseClicked((_: MouseEvent) => {
-    val selectedItem = indicatorListView.getSelectionModel.getSelectedItem
-    if (selectedItem != null) {
-      val ndx = indicatorList.indexWhere(b => b.id == selectedItem.id)
-      if (ndx != -1) {
-        indicatorList.remove(ndx)
-        indicatorListView.getSelectionModel.clearSelection()
-        bundleController.map(_.removeStixFromBundle(selectedItem))
-      }
-    }
-  })
-
-  // for use in indicatorListView, to allow editing (double click) of the list entries text, the indicator name
-  val indicatorStringConverter = new StringConverter[IndicatorForm] {
-
-    def fromString(newName: String): IndicatorForm = {
-      val selectedStix = indicatorListView.getSelectionModel.getSelectedItem
-      if (selectedStix != null) {
-        // remove the old stix from the bundleController
-        bundleController.map(_.removeStixFromBundle(selectedStix))
-        // put the new name in the selection
-        selectedStix.name.value = newName
-        // add the new selection to the bundleController
-        bundleController.map(_.addStixToBundle(selectedStix))
-      }
-      selectedStix
-    }
-
-    def toString(stix: IndicatorForm): String = stix.name.value
   }
 
 }
